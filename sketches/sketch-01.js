f9449813e984aeea2f741a9b171c7f948893479e
Vitali Yanusheuski <butuk@users.noreply.github.com>
2024-05-09 12:15:22 +0200

    feat: first figures

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,13 +1,24 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    const x = width * .35;
+    const y = height * .35;
+    const w = width * .3;
+    const h = width * .3;
+
+    context.fillStyle = 'red';
+    context.beginPath();
+    context.fillRect(x, y, w, h);
+
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width * .1, height * .1);
+
+
   };
 };
 
