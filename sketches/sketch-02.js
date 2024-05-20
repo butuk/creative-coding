9102665aeb607887c7021c29bf2b26d03894d5bb
Vitali Yanusheuski <butuk@users.noreply.github.com>
2024-05-20 10:54:21 +0200

    feat: add dot agent to the canvas

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -8,7 +8,37 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    const point = {x: 800, y: 400, radius: 10};
+    context.save();
+    context.beginPath();
+    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
+    context.fillStyle = 'black';
+    context.fill();
+    context.restore();
+
+    const pointB = new Point(600, 500, 10);
+
+    pointB.draw(context);
   };
 };
 
 canvasSketch(sketch, settings);
+
+class Point {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.context = context;
+    this.draw();
+  }
+
+  draw(context) {
+   // console.log(context);
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fillStyle = 'black';
+    context.fill();
+  }
+}
