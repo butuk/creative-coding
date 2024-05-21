ad2cc0c6531731093f5dc50767f3f877c441171e
Vitali Yanusheuski <butuk@users.noreply.github.com>
2024-05-21 10:33:57 +0200

    feat: add random amout of dots on the canvas

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,41 +1,43 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+
+const amount = 40;
+const pointRadius = 10;
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+
+
+  const points = [];
+  for (let i=0; i<amount; i++) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+    points.push(new Point(x, y));
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const point = {x: 800, y: 400, radius: 10};
-    context.save();
-    context.beginPath();
-    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
-    context.fill();
-    context.restore();
-
-    const pointB = new Point(600, 500, 10);
-
-    pointB.draw(context);
+    points.forEach(point => {
+      point.draw(context);
+    })
   };
 };
 
 canvasSketch(sketch, settings);
 
 class Point {
-  constructor(x, y, radius) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
-    this.context = context;
-    this.draw();
+    this.radius = pointRadius;
   }
 
   draw(context) {
-   // console.log(context);
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fillStyle = 'black';
