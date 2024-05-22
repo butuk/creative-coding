e3351dcbaec3bd1f6366e1a5b8fac562edf73bd6
Vitali Yanusheuski <butuk@users.noreply.github.com>
2024-05-22 11:00:00 +0200

    feat: add dots animation

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -5,7 +5,8 @@ const amount = 40;
 const pointRadius = 10;
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
 };
 
 const sketch = ({ context, width, height }) => {
@@ -23,24 +24,47 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
     points.forEach(point => {
+      point.update();
       point.draw(context);
+      point.bounce(width, height);
     })
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = pointRadius;
+  }
+}
+
+class Point {
+  constructor(x, y) {
+    this.start = new Vector(x, y);
+    this.vector = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = pointRadius * random.range(.5, 1.5);
+  }
+
+  update() {
+    this.start.x += this.vector.x;
+    this.start.y += this.vector.y;
+  }
+
+  bounce(width, height) {
+    if (this.start.x <= 0 || this.start.x >= width) this.vector.x *= -1;
+    if (this.start.y <= 0 || this.start.y >= height) this.vector.y *= -1;
   }
 
   draw(context) {
+    context.save();
+    context.translate(this.start.x, this.start.y);
     context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.lineWidth = 4;
+    context.stroke();
+    context.restore();
   }
 }
