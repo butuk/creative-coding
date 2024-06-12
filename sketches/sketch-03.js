0ba563d1ca529a9406a0a15028016888bc66a8ef
Vitali Yanusheuski <butuk@users.noreply.github.com>
2024-06-12 11:01:29 +0200

    feat: add more inputs to the ui-panel

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -11,6 +11,13 @@ const settings = {
 const parameters = {
   columns: 10,
   rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  frequency: 0.001,
+  amplitude: 0.25,
+  animate: true,
+  frame: 0,
+  lineCap: 'butt'
 }
 
 const animationSpeed = 20;
@@ -41,6 +48,8 @@ const sketch = () => {
       const column = i % grid.columns;
       const row = Math.floor(i / grid.columns);
 
+      let f = parameters.animate ? frame : parameters.frame;
+
       const item = {
         x: column * cell.width,
         y: row * cell.height,
@@ -48,10 +57,11 @@ const sketch = () => {
         height: cell.height * .9,//
       }
 
-      item.randomNumber = random.noise2D(item.x + frame * animationSpeed, item.y, 0.001);
-      item.angle = item.randomNumber * Math.PI * .25;
+      //item.randomNumber = random.noise2D(item.x + frame * animationSpeed, item.y, parameters.frequency);
+      item.randomNumber = random.noise3D(item.x, item.y, f * animationSpeed, parameters.frequency);
+      item.angle = item.randomNumber * Math.PI * parameters.amplitude;
       //console.log(math.mapRange(item.randomNumber, -1, 1, 1, 30)); //------------------
-      item.scale = math.mapRange(item.randomNumber, -1, 1, 1, 30);
+      item.scale = math.mapRange(item.randomNumber, -1, 1, parameters.scaleMin, parameters.scaleMax);
 
       context.save();
       context.translate(pic.marginX, pic.marginY)
@@ -59,6 +69,7 @@ const sketch = () => {
       context.translate(item.width / 2, item.height / 2);
       context.rotate(item.angle);
       context.lineWidth = item.scale;
+      context.lineCap = parameters.lineCap
       context.beginPath();
 
       context.moveTo(0, 0);
@@ -70,10 +81,24 @@ const sketch = () => {
 };
 const createPane = () => {
   const pane = new Tweakpane.Pane();
+  let folder;
 
-  let folder = pane.addFolder({title: 'Grid'});
+  folder = pane.addFolder({title: 'Grid'});
+  folder.addInput(parameters, 'lineCap', {options: {butt: 'butt', round: 'round', square: 'square'}});
   folder.addInput(parameters, 'columns', {min: 2, max: 50, step: 1});
   folder.addInput(parameters, 'rows', {min: 2, max: 50, step: 1});
+  folder.addInput(parameters, 'scaleMin', {min: 1, max: 100});
+  folder.addInput(parameters, 'scaleMax', {min: 1, max: 100});
+
+  folder = pane.addFolder({title: 'Noise'});
+  folder.addInput(parameters, 'frequency', {min: -0.01, max: 0.01});
+  folder.addInput(parameters, 'amplitude', {min: 0, max: 1});
+
+  folder = pane.addFolder({title: 'Play'});
+  folder.addInput(parameters, 'animate');
+  folder.addInput(parameters, 'frame', {min: 0, max: 999, step: 1});
+
+
 };
 createPane();
 
