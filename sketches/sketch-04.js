9670879b8dff4f1895591faf1925be67688cf50e
Vitali Yanusheuski <butuk@users.noreply.github.com>
2024-06-20 11:27:27 +0200

    feat: make letter be measured

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -3,16 +3,44 @@ const canvasSketch = require('canvas-sketch');
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
+const font = {
+  size: '400px',
+  family: 'serif'
+};
+const text = 'R';
 
 const sketch = () => {
   return ({ context, width, height }) => {
+
+    //Artboard
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+    //Big letter
     context.fillStyle = 'black';
-    context.font = '400px serif';
-    context.textBaseline = 'top';
-    context.fillText('A', 0, 0);
+    context.font = `${font.size} ${font.family}`;
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+
+    //Text measurements
+    const metrics = context.measureText(text);
+    const img = {
+      x: -metrics.actualBoundingBoxLeft,
+      y: -metrics.actualBoundingBoxAscent,
+      width: metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight,
+      height: metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent,
+    }
+
+    console.log(metrics, img.y); //---------------------
+
+    context.save();
+    context.translate((width - img.width) * .5 - img.x, (height - img.height) * .5 - img.y);
+    context.beginPath();
+    context.rect(img.x, img.y, img.width, img.height);
+    context.stroke();
+    context.fillText(text, 0, 0);
+    context.restore();
+
   };
 
 };
